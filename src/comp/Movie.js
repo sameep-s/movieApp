52cdc6bcf3175201b3ab90a3f63ef3aca4d4a061
Sameep Sharma <36707002+sameep-s@users.noreply.github.com>
2021-05-15 23:43:48 +0530

    Update Movie.js

--- src/comp/Movie.js
+++ src/comp/Movie.js
@@ -2,7 +2,6 @@ import React from 'react';
 import "../index.css";
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
-const temp = 'https://images.unsplash.com/photo-1598899134739-24c46f58b8c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1038&q=80';
 
 const setVoteClass = (vote) => {
     if (vote >= 8) {
